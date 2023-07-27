ab742aec50db14457c7e467ce15a813dbae25e46
Matheus Santos <matheus1819.santos20@gmail.com>
2023-07-27 19:38:25 -0300

    Working with uploads and testing some global variables

---
+++ class_59_file_uploads/src/App/Classes/Home.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Classes;
+
+class Home {
+    public function index(): string {
+        // <input type = 'file' name='receipt'/>
+        // <input type = 'file' name='myImage'/>
+        return "
+            <form action='/upload' method='post' enctype='multipart/form-data'>
+            <input type = 'file' name='receipt[]'/>
+            <input type = 'file' name='receipt[]'/>
+            <button type = 'submit'> Upload</button>
+            </form>
+            ";
+    }
+
+    public function upload() {
+        echo '<pre>';
+        var_dump($_FILES);
+
+        echo '</pre>';
+        
+        // $file_path = STORAGE_PATH . '/' . $_FILES['receipt']['name'];
+        
+        // move_uploaded_file(
+        //     $_FILES['receipt']['tmp_name'], $file_path   
+        // );
+        
+        // echo '<pre>';
+        // var_dump(pathinfo($file_path));
+        // echo '</pre>';
+    }
+}
